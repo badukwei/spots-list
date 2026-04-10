6ccaba0fce3225a1f52a253ace268d459248ccdb
badukwei <linwgpeter@gmail.com>
2026-04-10 15:34:16 +0800

    fix: close postgres connection pool on shutdown

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,23 +1,38 @@
-import { Global, Module } from '@nestjs/common'
+import { Global, Inject, Module, OnApplicationShutdown } from '@nestjs/common'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 import 'dotenv/config'
 
 export const DATABASE = 'DATABASE'
+export const PG_CLIENT = 'PG_CLIENT'
 export type DrizzleDB = ReturnType<typeof drizzle<typeof schema>>
 
 @Global()
 @Module({
   providers: [
     {
-      provide: DATABASE,
+      provide: PG_CLIENT,
       useFactory: () => {
-        const client = postgres(process.env.DATABASE_URL!)
-        return drizzle(client, { schema })
+        if (!process.env.DATABASE_URL) {
+          throw new Error('DATABASE_URL is not set')
+        }
+        return postgres(process.env.DATABASE_URL)
       },
+    },
+    {
+      provide: DATABASE,
+      useFactory: (client: ReturnType<typeof postgres>) =>
+        drizzle(client, { schema }),
+      inject: [PG_CLIENT],
     },
   ],
   exports: [DATABASE],
 })
-export class DbModule {}
+export class DbModule implements OnApplicationShutdown {
+  constructor(@Inject(PG_CLIENT) private readonly client: ReturnType<typeof postgres>) {}
+
+  async onApplicationShutdown() {
+    await this.client.end()
+  }
+}
